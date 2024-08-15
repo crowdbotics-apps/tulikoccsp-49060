0fd1d4b3fe840ea3c8b1a7d4ada5d0c86fbc0e9b
crowdbotics-apps <team@crowdbotics.com>
2024-08-15 00:39:54 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AppInfo
+
+admin.site.register(AppInfo)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,16 @@
+from django.conf import settings
 from django.db import models
+
+
+class AppInfo(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=255,
+    )
+    description = models.TextField()
+    version = models.CharField(
+        max_length=50,
+    )
+    last_updated = models.DateTimeField(
+        auto_now=True,
+    )
